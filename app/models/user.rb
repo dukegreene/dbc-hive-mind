407b4abb02cfdfc2a615a0391a4510509bb50bea
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-06-29 20:59:17 -0500

    add cohort and feels associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
   
+  belongs_to :cohort
+  has_many :feels, class_name: "Emoting"
+  has_many :emotions, through: :feels
 end
