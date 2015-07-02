a016474218946891b399de3d96748fa9acfe0acb
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-07-01 20:44:14 -0500

    add fields to emotion_surveys

--- db/migrate/20150701201751_create_emotion_surveys.rb
+++ db/migrate/20150701201751_create_emotion_surveys.rb
@@ -1,4 +1,11 @@
 class CreateEmotionSurveys < ActiveRecord::Migration
   def change
+    create_table :emotion_surveys do |t|
+      t.string :title
+      t.integer :cohort_id
+      t.integer :creator_id
+
+      t.timestamps
+    end
   end
 end
