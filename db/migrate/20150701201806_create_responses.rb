64fd05114ccaa18d58e80d1e249676ad8b997cf1
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-07-01 20:48:04 -0500

    add fields to responses

--- db/migrate/20150701201806_create_responses.rb
+++ db/migrate/20150701201806_create_responses.rb
@@ -1,4 +1,11 @@
 class CreateResponses < ActiveRecord::Migration
   def change
+    create_table :responses do |t|
+      t.integer :respondent_id
+      t.integer :prompt_id
+      t.integer :selected_intensity
+
+      t.timestamps
+    end
   end
 end
