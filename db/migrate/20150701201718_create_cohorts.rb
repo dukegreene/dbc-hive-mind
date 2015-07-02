b4a6fbd8a3802da7dd9937b6ecebe935867dd87b
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-07-01 20:21:11 -0500

    add fields to cohorts

--- db/migrate/20150701201718_create_cohorts.rb
+++ db/migrate/20150701201718_create_cohorts.rb
@@ -1,4 +1,9 @@
 class CreateCohorts < ActiveRecord::Migration
   def change
+    create_table :cohorts do |t|
+      t.string :name
+      t.string :mascot_image_url
+
+      t.timestamps
   end
 end
