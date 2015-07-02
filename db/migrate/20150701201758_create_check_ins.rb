320519965841a3610f9476a70ecbde978efee3b8
Duke Greene <Duke@Dukes-MacBook-Pro.local>
2015-07-01 20:46:36 -0500

    add fields to check_ins

--- db/migrate/20150701201758_create_check_ins.rb
+++ db/migrate/20150701201758_create_check_ins.rb
@@ -1,4 +1,11 @@
 class CreateCheckIns < ActiveRecord::Migration
   def change
+    create_table :check_ins do |t|
+      t.integer :self_checker_id
+      t.integer :emotion_survey_id
+      t.string :self_check_text
+
+      t.timestamps
+    end
   end
 end
